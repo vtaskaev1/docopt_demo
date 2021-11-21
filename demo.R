816592e92fb05b87f2048a5b1da33e54407b6e92
Vadim Taskaev <vadim.taskaev@outlook.com>
2021-11-20 20:02:21 -0800

    Checkpoint nov20 2002

--- demo.R
+++ demo.R
@@ -2,16 +2,19 @@
 # date: 2020-01-15
 
 "This script prints out docopt args.
-Usage: demo.R <arg1> [<arg4>] --arg2=<arg2> [--arg3=<arg3>]
+Usage: demo.R <arg1> --arg2=<arg2> [--arg3=<arg3>]
 
 Options:
-<arg>             Takes any value (this is a required positional argument)
-[<arg4>]          Takes any value (this is an optional positional argument)
+<arg1>             Takes any value (this is a required positional argument)
 --arg2=<arg2>     Takes any value (this is a required option)
 [--arg3=<arg3>]   Takes any value (this is an optional option)
 " -> doc
 
 library(docopt)
 opt <- docopt(doc)
-print(opt)
-print(typeof(opt))
+#print(opt)
+#print(typeof(opt))
+
+main <- function(opt) {
+  print(opt)
+}
