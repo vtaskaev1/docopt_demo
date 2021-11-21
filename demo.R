d3a00b5bc7462a5d7a22e1243a0d9d9be6d372bf
Vadim Taskaev <vadim.taskaev@outlook.com>
2021-11-21 09:23:18 -0800

    Exercise 5 completed

--- demo.R
+++ demo.R
@@ -2,19 +2,17 @@
 # date: 2020-01-15
 
 "This script prints out docopt args.
-Usage: demo.R <arg1> --arg2=<arg2> [--arg3=<arg3>]
+Usage: demo.R <arg1> [<arg4>] --arg2=<arg2> [--arg3=<arg3>]
 
 Options:
-<arg1>             Takes any value (this is a required positional argument)
+<arg1>            Takes any value (this is a required positional argument)
+[<arg4>]          Takes any value (this is an optional positional argument)
 --arg2=<arg2>     Takes any value (this is a required option)
 [--arg3=<arg3>]   Takes any value (this is an optional option)
 " -> doc
 
 library(docopt)
 opt <- docopt(doc)
-#print(opt)
-#print(typeof(opt))
-
-main <- function(opt) {
-  print(opt)
-}
+print(opt)
+print(typeof(opt))
+print(opt$arg4)
